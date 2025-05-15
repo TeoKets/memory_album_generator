95efe11644bd22464adfa66d2696e072d7d2b24e
Theofilos Ketsetzidis <ketsetzidis909@gmail.com>
2025-05-15 18:31:50 +0300

    Project: scrap the top 100 songs of a date you input and making a spotify playlist through that
    skills used: web-scrapping,API request, authentication,automation
    
    ready for nostalgia?

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ sp=spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id,
 #making the list for inputing into the method to add songs into the playlist
 inputing_songs_list=[]
 for song in top_100_songs_list:
-    song=sp.search(q=(song,),limit=1,type="track")
+    song=sp.search(q=(song,year),limit=1,type="track")
     inputing_songs_list.append(song["tracks"]["items"][0]["external_urls"]["spotify"])
 
 playlist=sp.user_playlist_create(user=user_id,name=f"{year}'s TOP 100 HITS",description="ready for nostalgia?",public=False)
